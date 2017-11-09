fd1e408a7445e06ea45dd57d55b28b2f75782e8c
Felix Törnqvist <felix.trnqvist@gmail.com>
2017-11-09 13:38:38 +0100

    task 1.1 done

--- 1.1/app/src/main/java/com/example/uppgift11/MainActivity.java
+++ 1.1/app/src/main/java/com/example/uppgift11/MainActivity.java
@@ -1,26 +1,44 @@
 package com.example.uppgift11;
 
-import android.content.Intent;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.EditText;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
-    public static final String EXTRA_MESSAGE = "com.example.uppgift11.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
+        String[] listItems = {"Hello", "world!", "this", "is ", "a", "test", "press the plus-button to activate dialog"};
+        ListView todoList = (ListView) findViewById(R.id.todo_list);
+        todoList.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, listItems));
     }
 
-    /** Called when the user taps the send button*/
-    public void sendMessage(View view) {
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.editText);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
+    /**
+     * Called when the floating action button is pressed
+     */
+    public void fabClick(View view) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+            }
+        });
+
+        builder.setMessage("To be continued...");
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 }
