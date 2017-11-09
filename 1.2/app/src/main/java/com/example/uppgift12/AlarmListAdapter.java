b5159a55440b43e77b11c440ccce31b89db718f3
Felix Törnqvist <felix.trnqvist@gmail.com>
2017-11-09 16:21:17 +0100

    1.2 progress..

---
+++ 1.2/app/src/main/java/com/example/uppgift12/AlarmListAdapter.java
@@ -0,0 +1,32 @@
+package com.example.uppgift12;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.BaseAdapter;
+
+/**
+ * Created by felix on 2017-11-09.
+ */
+
+public class AlarmListAdapter extends BaseAdapter {
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        return null;
+    }
+}
--- 1.2/app/src/main/java/com/example/uppgift12/MainActivity.java
+++ 1.2/app/src/main/java/com/example/uppgift12/MainActivity.java
@@ -1,15 +1,25 @@
 package com.example.uppgift12;
 
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.app.TimePickerDialog;
 import android.content.DialogInterface;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.text.format.DateFormat;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TimePicker;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private ArrayList<Time> alarmTimes = new ArrayList<>();
+    private ArrayAdapter<Time> alarmTimesAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,26 +29,66 @@ public class MainActivity extends AppCompatActivity {
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
 
-        String[] listItems = {"Hello", "world!", "this", "is ", "a", "test", "press the plus-button to activate dialog"};
         ListView todoList = (ListView) findViewById(R.id.todo_list);
-        todoList.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, listItems));
+        alarmTimesAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, alarmTimes);
+        todoList.setAdapter(alarmTimesAdapter);
     }
 
     /**
      * Called when the floating action button is pressed
      */
     public void fabClick(View view) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-
-        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+        TimePickerDialog.OnTimeSetListener setListener = new TimePickerDialog.OnTimeSetListener() {
             @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
+            public void onTimeSet(TimePicker timePicker, int i, int i1) {
+                alarmTimes.add(new Time(i, i1));
+                alarmTimesAdapter.notifyDataSetChanged();
+                Log.v("Main", alarmTimes.toString());
             }
-        });
+        };
 
-        builder.setMessage("To be continued...");
+        TimePickerFragment timePickerFrag = new TimePickerFragment();
+        timePickerFrag.setOnTimeSetListener(setListener);
+        timePickerFrag.show(getFragmentManager(), "timePicker");
+    }
 
-        AlertDialog dialog = builder.create();
-        dialog.show();
+    /**
+     * Time picker fragment dialog (fragment because it's suppose to make the dialog stay up when the phone is rotated).
+     */
+    public static class TimePickerFragment extends DialogFragment {
+        private TimePickerDialog.OnTimeSetListener listener;
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            return new TimePickerDialog(getActivity(), listener, 0, 0, true);
+        }
+
+        public void setOnTimeSetListener(TimePickerDialog.OnTimeSetListener listener) {
+            this.listener = listener;
+        }
+    }
+
+    /**
+     * Used to neatly store hour and minute together.
+     */
+    private class Time {
+        private int hour, minute;
+
+        public Time(int hour, int minute) {
+            this.hour = hour;
+            this.minute = minute;
+        }
+
+        public int getHour() {
+            return hour;
+        }
+
+        public int getMinute() {
+            return minute;
+        }
+
+        public String toString() {
+            return hour + ":" + minute;
+        }
     }
 }
