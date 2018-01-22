38f5e5bb48caeb50153f96cf575b999d4f3ab1e5
Felix Törnqvist <felix.trnqvist@gmail.com>
2018-01-22 21:56:38 +0100

    4.1.1 started and completed

---
+++ 411/app/src/main/java/com/example/uppgift411YoutubeLinker/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.uppgift411YoutubeLinker;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+/**
+ * Main Activity for task 4.1.1, an app with links to YouTube videos.
+ */
+public class MainActivity extends AppCompatActivity {
+    String[] ytLinks = new String[]{
+            "https://www.youtube.com/watch?v=II3AXSpwsMk",
+            "https://www.youtube.com/watch?v=m8KzmlIEsHs",
+            "https://www.youtube.com/watch?v=xe-f4gokRBs",
+            "https://www.youtube.com/watch?v=VS8wlS9hF8E",
+            "https://www.youtube.com/watch?v=rsXQInxxzBU",
+            "https://www.youtube.com/watch?v=r6Rp-uo6HmI",
+            "https://www.youtube.com/watch?v=iYx10BWoL58"
+    };
+    ListView ytListView;
+    ArrayAdapter ytLinkAdapter;
+
+    /**
+     * Initializes a ListView with the links with a listener
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ytLinkAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, ytLinks);
+        ytListView = findViewById(R.id.list_main_linkList);
+
+        ytListView.setAdapter(ytLinkAdapter);
+        ytListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                openYTLink(ytLinks[i]);
+            }
+        });
+    }
+
+    /**
+     * Opens specified link in YouTube app or gives the user options of what app to open if YouTube
+     * isn't installed.
+     * @param link YouTube link to open
+     */
+    private void openYTLink(String link) {
+        Uri uri = Uri.parse(link);
+        uri = Uri.parse("vnd.youtube:" + uri.getQueryParameter("v"));
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
+    }
+}
