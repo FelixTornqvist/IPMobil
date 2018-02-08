ed4abefe0466f0382e01179e17de9e1c16d44d1b
Felix Törnqvist <felix.trnqvist@gmail.com>
2018-02-08 12:21:49 +0100

    7.2.1 dialog (and toast ?) complete

--- 7.2.1/app/src/main/java/com/example/uppgift721popupmadness/MainActivity.java
+++ 7.2.1/app/src/main/java/com/example/uppgift721popupmadness/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.uppgift721popupmadness;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,7 +21,34 @@ public class MainActivity extends AppCompatActivity {
      * @param v not used, required to register listener in layout file.
      */
     public void createToastListener(View v) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(R.string.main_dialog_toast_title);
+        final EditText messageET = new EditText(this);
+        builder.setView(messageET);
 
+        builder.setPositiveButton(R.string.all_ok, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                sendToast(messageET.getText().toString());
+            }
+        });
+
+        builder.setNegativeButton(R.string.all_cancel, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.cancel();
+            }
+        });
+
+        builder.show();
+    }
+
+    /**
+     * Creates a toast message.
+     * @param s message to send
+     */
+    private void sendToast(String s) {
+        Toast.makeText(this, s, Toast.LENGTH_LONG).show();
     }
 
     /**
@@ -26,6 +57,33 @@ public class MainActivity extends AppCompatActivity {
      * @param v not used, required to register listener in layout file.
      */
     public void createNotificationListener(View v) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(R.string.main_dialog_notification_title);
+        final EditText messageET = new EditText(this);
+        builder.setView(messageET);
 
+        builder.setPositiveButton(R.string.all_ok, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                sendNotification(messageET.getText().toString());
+            }
+        });
+
+        builder.setNegativeButton(R.string.all_cancel, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.cancel();
+            }
+        });
+
+        builder.show();
+    }
+
+    /**
+     * Creates a notification
+     * @param msg message that the notification will contain.
+     */
+    private void sendNotification(String msg) {
+        //TODO: IMPLEMENT!!...........................
     }
 }
