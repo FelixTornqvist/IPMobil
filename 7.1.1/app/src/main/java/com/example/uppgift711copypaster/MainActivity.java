8127ae9caf3ec39312e360529f7c8cdf79c272c2
Felix Törnqvist <felix.trnqvist@gmail.com>
2018-02-08 10:55:57 +0100

    7.1.1 initial setup

--- 7.1.1/app/src/main/java/com/example/uppgift711copypaster/MainActivity.java
+++ 7.1.1/app/src/main/java/com/example/uppgift711copypaster/MainActivity.java
@@ -2,12 +2,37 @@ package com.example.uppgift711copypaster;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    EditText toCopyET;
+    TextView pastedTW;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toCopyET = findViewById(R.id.edit_main_tocopy);
+        pastedTW = findViewById(R.id.text_main_pasted_text);
+    }
+
+    /**
+     * Listener for the "copy" button. Copies the text in toCopyET to the clipboard.
+     * @param v not used, required to register listener in layout file.
+     */
+    public void copyBtnListener(View v) {
+
+    }
+
+
+    /**
+     * Listener for the "paste" button. Pastes the content in clipboard to pastedTW.
+     * @param v not used, required to register listener in layout file.
+     */
+    public void pasteBtnListener(View v) {
+
     }
 }
